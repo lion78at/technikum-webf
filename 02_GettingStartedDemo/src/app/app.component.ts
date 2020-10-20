913a2429fec63376de38bfcfefd027c7768edd26
lion78at <b.loewenstein@gmx.at>
2020-10-20 19:33:49 +0200

    GettingStartedDemo finished

--- 02_GettingStartedDemo/src/app/app.component.ts
+++ 02_GettingStartedDemo/src/app/app.component.ts
@@ -3,10 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ title }}</h1>
+    <h1>BMI-Calculator</h1>
+
+    <p>
+      <label>Größe:
+        <input [(ngModel)]="height" type="number" (input)="resetBMI()">
+      </label> cm
+    </p>
+
+    <p>
+      <label>Gewicht:
+        <input [(ngModel)]="weight" type="number" (input)="resetBMI()">
+      </label> kg
+    </p>
+
+    <p>
+      <button (click)="calculateBMI()">BMI berechnen</button>
+    </p>
+
+    <div *ngIf="bmi">
+      <h2>Dein BMI: {{ bmi | number:'.2-2' }}</h2>
+      <img [src]="animal">
+    </div>
   `,
-  styles: []
+  styles: [`
+    h1 {
+      color: red;
+    }
+
+    img {
+      height: 200px;
+      width: auto;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'Hello World!';
+  height = 175;
+  weight = 75;
+  bmi: number;
+  animal: string;
+
+  calculateBMI(): void {
+    console.log(`height=${this.height}, weight=${this.weight}`);
+
+    this.bmi = this.weight / (this.height / 100) ** 2;
+
+    if (this.bmi < 20) {
+      this.animal = 'assets/giraffe.jpg';
+    }
+    else if (this.bmi < 25) {
+      this.animal = 'assets/horse.jpg';
+    }
+    else {
+      this.animal = 'assets/elephant.jpg';
+    }
+  }
+
+  resetBMI(): void {
+    this.bmi = null;
+  }
 }
