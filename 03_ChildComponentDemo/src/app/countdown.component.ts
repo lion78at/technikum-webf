dd8dc41dc695e36eb2475f63da8fd61af4140c2a
lion78at <b.loewenstein@gmx.at>
2020-11-10 18:03:53 +0100

    ChildComponentDemo changed

--- 03_ChildComponentDemo/src/app/countdown.component.ts
+++ 03_ChildComponentDemo/src/app/countdown.component.ts
@@ -3,17 +3,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-countdown',
   template: `
-    <h2>{{ text }}</h2>
-    <button (click)="buttonPressed()">Drück mich!</button>
+
   `,
   styles: [
   ]
 })
 export class CountdownComponent {
-  @Input() text: string;
-  @Output() pressed = new EventEmitter<number>();
-
-  buttonPressed(): void {
-    this.pressed.emit(42);   // feuert Event
-  }
+ 
 }
