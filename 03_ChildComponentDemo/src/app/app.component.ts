5cb61e6460aaf9bdedfb557b0ce6db63b9edcd2a
lion78at <b.loewenstein@gmx.at>
2020-11-10 19:14:48 +0100

    ChildComponentDemo finished

--- 03_ChildComponentDemo/src/app/app.component.ts
+++ 03_ChildComponentDemo/src/app/app.component.ts
@@ -3,10 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-countdown></app-countdown>
 
+    <app-countdown
+      time="3"
+      buttonLabel="Countdown starten"
+      (timeChange)="countdownTimeChanged($event)"
+      (finish)="countdownFinished()"
+    ></app-countdown>
   `,
   styles: []
 })
 export class AppComponent {
+  countdownTimeChanged(time: number): void {
+    console.log(`Die Zeit hat sich geändert: ${time}`);
+  }
 
+  countdownFinished(): void {
+    alert('Fertig!');
+  }
 }
--- 03_ChildComponentDemo/src/app/countdown.component.ts
+++ 03_ChildComponentDemo/src/app/countdown.component.ts
@@ -1,13 +1,44 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-countdown',
   template: `
+    <div *ngIf="!running">
+      <input [(ngModel)]="time" type="number" min="1" max="600">
+      <button (click)="startCountdown()">{{ buttonLabel }}</button>
+    </div>
 
+    <div *ngIf="running">
+      {{ time }}
+    </div>
   `,
   styles: [
   ]
 })
 export class CountdownComponent {
- 
+  @Input() buttonLabel = 'Starten';
+  @Input() time = 30;
+  @Output() timeChange = new EventEmitter<number>();
+  @Output() finish = new EventEmitter();
+  running = false;
+
+  startCountdown(): void {
+    this.running = true;
+    const startTime = this.time;
+
+    const subscription = timer(1000, 1000).subscribe(
+      tick => {
+        this.time--;
+        this.timeChange.emit(this.time);
+
+        if (this.time <= 0) {
+          subscription.unsubscribe();
+          this.running = false;
+          this.time = startTime;
+          this.finish.emit();
+        }
+      }
+    );
+  }
 }
