d0668ba02a12b51ca62de20330edade330ebc85f
lion78at <b.loewenstein@gmx.at>
2020-12-22 16:57:15 +0100

    stub for RESTDemo added

---
+++ 08_RESTDemo/Java/src/main/java/at/technikumwien/DBInitializer.java
@@ -0,0 +1,22 @@
+package at.technikumwien;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Configuration
+public class DBInitializer {
+	@Autowired
+	private NewsRepository newsRepository;
+		
+	@EventListener(ApplicationReadyEvent.class)
+	public void handleApplicationReady() {
+		newsRepository.saveAll(List.of(
+			new News("Hello World!", "Herzlich willkommen am Planeten Erde."),
+			new News("News-Portal online!", "Unser neues News-Portal ist online.")
+		));
+	}
+}
