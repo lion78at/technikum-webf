bdf38736568fe02a5045ab97a2c07d9e28e4cbd7
lion78at <b.loewenstein@gmx.at>
2021-09-07 19:18:36 +0200

    javascript/02 finished

--- Sourcen/01_Basics/JavaScript/02/app.js
+++ Sourcen/01_Basics/JavaScript/02/app.js
@@ -1,6 +1,13 @@
-function calculate(nr1, nr2, operation) {
+// concepts: arrow function, callback
+
+function calculate(nr1, nr2, operation, callback) {
     const result = operation(nr1, nr2);
-    return result;
+    if (callback) {
+        callback(result);
+    }
+    else {
+        return result;
+    }
 }
 
 // variant 1: concrete function
@@ -8,16 +15,36 @@ function operateSum(a, b) {
     return a + b;
 }
 
-const result1 = calculate(
-    1,
-    3,
-    operateSum
-);
-console.log(result1);
+function showResult(res) {
+    console.log("Ergebnis : " + res);
+}
 
-const result2 = calculate(
+calculate(
     1,
     3,
-    operateSum
+    operateSum,
+    showResult
 );
-console.log(result2);
+
+// variant 2: anonymous function
+calculate(
+    1,
+    3,
+    function(a, b) {
+        return a + b;
+    },
+    function(res) {
+        console.log("Ergebnis : " + res);
+    }
+);
+
+// variant 3: arrow function
+calculate(
+    1,
+    3,
+    (a, b) => a + b,
+    res => console.log("Ergebnis : " + res)
+);
+
+// const add = (x, y, z) => x + y + z;
+// console.log(add(1, 2, 3));
